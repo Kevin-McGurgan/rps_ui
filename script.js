665faa81ea713aa5a6afd60398fdbb5e8abcb12f
Kevin McGurgan <kevin.mcgurgan@gmail.com>
2019-08-09 18:35:02 -0400

    Initial commit for rock, paper, scissors with a ui.

---
+++ script.js
@@ -0,0 +1,85 @@
+// TODO - End the game when someone gets to 3
+
+let playerScore = 0;
+let computerScore = 0;
+let roundCount = 0;
+
+// Randomly generate the computer's play
+function getComputerSelection () {
+    const computerOptions = ["rock", "paper", "scissors"];
+    return computerOptions[Math.floor(Math.random()*3)];
+}
+
+
+// Keep score and determine winner
+function game () {
+    while (roundCount < 5) {
+        if (playerScore == 3) {
+            return "Great job! You beat the computer.";
+        } else if (computerScore == 3) {
+            return "The computer won. Better luck next time.";
+        }
+    }
+}
+
+const rockBtn = document.querySelector("#rock");
+rockBtn.addEventListener("click", () => {
+  playRound("rock", getComputerSelection());
+});
+
+const paperBtn = document.querySelector("#paper");
+paperBtn.addEventListener("click", () => {
+  playRound("paper", getComputerSelection());
+});
+
+const scissorsBtn = document.querySelector("#scissors");
+scissorsBtn.addEventListener("click", () => {
+  playRound("scissors", getComputerSelection());
+});
+
+// initialize dynamic game content
+const pScore = document.querySelector("#playerScore");
+
+const addPScore = document.createElement("p");
+  addPScore.classList.add("numeral");
+  addPScore.textContent = playerScore.toString();
+  pScore.appendChild(addPScore);
+
+const cScore = document.querySelector("#computerScore");
+
+const addCScore = document.createElement("p");
+  addCScore.classList.add("numeral");
+  addCScore.textContent = computerScore.toString();
+  cScore.appendChild(addCScore);
+
+const banner = document.querySelector("#infoBanner")
+
+const initBanner = document.createElement("p");
+  initBanner.textContent = "Play best of 5 against the computer."
+  banner.appendChild(initBanner);
+
+
+// play a single round
+function playRound (playerSelection, computerSelection) {
+    // player wins
+    if (playerSelection == "rock" && computerSelection == "scissors" ||
+        playerSelection == "paper" && computerSelection == "rock" ||
+        playerSelection == "scissors" && computerSelection == "paper")  {
+        ++playerScore;
+        addPScore.textContent = playerScore.toString();
+        initBanner.textContent = "You win! " + playerSelection + " beats " + computerSelection;
+    }
+    // computer wins
+    else if (computerSelection == "rock" && playerSelection == "scissors" || 
+            computerSelection == "paper" && playerSelection == "rock" || 
+            computerSelection == "scissors" && playerSelection == "paper")  {
+        ++computerScore;
+        ++roundCount;
+        addCScore.textContent = computerScore.toString();
+        initBanner.textContent = "You lose. " + computerSelection + " beats " + playerSelection;
+    }
+    // Draw
+    else {
+      initBanner.textContent = "It\'s a draw. Try again.";
+    }
+}
